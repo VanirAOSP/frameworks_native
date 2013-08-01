96c524b065cd2eac93d000fc6c3e7bc52a5dcd4e
Amit Pundir <amit.pundir@linaro.org>
2013-08-01 18:15:27 +0530

    servicemanager: enable bctest with debug logs
    Enable bctest binder test and put ioctl pass/failure logs for lava.
    
    Change-Id: I33eb6b87d3f9efcef7334dbb0a649ce23b8b0a21
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- cmds/servicemanager/Android.mk
+++ cmds/servicemanager/Android.mk
@@ -1,9 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 
-#include $(CLEAR_VARS)
-#LOCAL_SRC_FILES := bctest.c binder.c
-#LOCAL_MODULE := bctest
-#include $(BUILD_EXECUTABLE)
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_CFLAGS := -DBCTEST_DEBUG=1
+LOCAL_SRC_FILES := bctest.c binder.c
+LOCAL_MODULE := bctest
+include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog
