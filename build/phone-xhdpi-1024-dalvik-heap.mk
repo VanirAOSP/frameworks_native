0949ed37f9780fe99195ee286ff2e5524807b451
MBQ- <MBQsniper@hotmail.com>
2013-08-28 12:24:28 -0700

    Correct phone-xhdpi-1024-dalvik-heap.mk heap size
    fixes issue: https://code.google.com/p/android/issues/detail?id=40961
    
    Change-Id: Ia9a3e8f9f4a2664c21792948147ed6c43b501acb
    Signed-off-by: nuclearmistake <nuclearmistake@gmail.com>

--- build/phone-xhdpi-1024-dalvik-heap.mk
+++ build/phone-xhdpi-1024-dalvik-heap.mk
@@ -19,7 +19,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=64m \
-    dalvik.vm.heapsize=256m \
+    dalvik.vm.heapsize=384m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
