5ef4e45f3ec217a3ab323813f12c5702f9ed5bb1
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2013-11-05 16:09:48 +0100

    Fix some char16_t vs. uint16_t bits
    Change-Id: Ie2392098fa6b25f3c0713a7774929e36d824d26f
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -1061,7 +1061,7 @@ String16 Parcel::readString16() const
 {
     size_t len;
     const char16_t* str = readString16Inplace(&len);
-    if (str) return String16(str, len);
+    if (str) return String16(reinterpret_cast<const uint16_t*>(str), len);
     ALOGE("Reading a NULL string not supported here.");
     return String16();
 }
