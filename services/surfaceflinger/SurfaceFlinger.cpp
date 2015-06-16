29789ca9f8582b56c14dd761b0766bc1fe15e79f
Ozzy2403 <oskar.kratochvil@gmx.de>
2015-06-16 20:01:32 +0200

    STE-patch
    Change-Id: If046bb08539dac0021021022b06b272d07a4f1c2

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3447,6 +3447,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
     uint32_t code;
     Parcel const* data;
     Parcel* reply;
+    Mutex mLock;
 
     enum {
         MSG_API_CALL,
@@ -3460,6 +3461,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
      */
     virtual status_t transact(uint32_t code,
             const Parcel& data, Parcel* reply, uint32_t /* flags */) {
+        mLock.lock();
         this->code = code;
         this->data = &data;
         this->reply = reply;
@@ -3477,6 +3479,7 @@ class GraphicProducerWrapper : public BBinder, public MessageHandler {
             looper->sendMessage(this, Message(MSG_API_CALL));
             barrier.wait();
         }
+        mLock.unlock();
         return result;
     }
 
